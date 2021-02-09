50316ef9a789d4758fb0d0d187519215dd4460f7
“siddiq76” <siddiq.khan0786@gmail.com>
2021-02-09 22:31:50 +0530

    added changes

---
+++ TicTacToe/src/TicTacUC1.java
@@ -0,0 +1,20 @@
+
+public class TicTacUC1 {
+
+	static  char[] board = new char[10];
+		
+	 
+public static char[]  creatingBoard() {
+
+
+	for (int i = 1; i < board.length; i++) {
+	board[i] = ' ';
+	}
+	return board;
+	}
+public static void main(String args[]) {
+
+	System.out.println("Welcome to tic tac board");
+	board = creatingBoard();
+		}
+	}
